ce16c1487fbf3061da71693c48cb4bd734228138
Daniel Martinez-Krams <danielmartinezkrams@students.berkeley.net>
2017-11-09 10:33:32 -0800

    api start

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{background: "#333333"}}>
-          <img src={logo}  alt="logo" style={{size:"200%", paddingTop:"100px"}}/>
-          <h1 className="App-title" style={{fontFamily:"Roboto, sans-serif", color: "#ffffff", size:"300%", fontWeight: "900", overflow: "visible", textAlign: "center", width: "70%", margin: "auto", lineHeight: "80%", paddingBottom: "8%", paddingTop: "2%"}}>ROAST MY TEACHER</h1>
+      <div className="App">
+          <img src={logo}  alt="logo" id="titleLogo" />
+          <h1 className="App-title">ROAST MY TEACHER</h1>
           <div className="confirm">
-              <p id="ver">Student Verification</p>
-              <input type="text" /><button id="con" onClick="confirm()">CONFIRM</button>
-
-
+              <h3 id="verification">Student Verification</h3>
+              <input type="text" id="idCheck"/><button id="confirmButton" onClick={confirm}>CONFIRM</button>
           </div>
       </div>
     )
   }
+
+}
+
+const app_url = "http://ec2-54-183-163-136.us-west-1.compute.amazonaws.com";
+//const access_token;
+//const fullPath;
+
+const $button = $("#idCheck");
+$button.click(function(){
+    const studentID = document.getElementById("confirm").value;
+    console.log(studentID);
+});
+
+function confirm() {
+    const studentID = document.getElementById("confirm").value;
+    console.log(studentID);
+    $.post(app_url + "/api/login", {"student_id": studentID.toString()}, function (data) {
+        access_token = data["access_token"];
+        console.log(data);
+        window.location.href = "/teachers.html?access_token=" + access_token;
+    }).fail(function (data) {
+        console.log(data);
+        window.location = "/error.html"
+    });
 }
 
 /*class Box extends React.Component{
--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
