f17c9e13ea06f2171161ce015a77d1c75ccf029a
Daniel Martinez-Krams <danielmartinezkrams@students.berkeley.net>
2018-02-22 09:31:41 -0800

    Post Complete

--- src/js/Review.js
+++ src/js/Review.js
@@ -5,7 +5,7 @@ class Review extends Component {
     render(){
         return(
             <div className="displayCircle">
-                {this.props.number}
+                <h1>{this.props.number}</h1>
             </div>
         )
     }
--- src/js/TeacherPage.js
+++ src/js/TeacherPage.js
@@ -7,13 +7,20 @@ class TeacherPage extends Component{
     constructor(props) {
         super(props);
         this.url = "https://roast-my-teacher-backend.herokuapp.com/api/roasts/";
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.getData = this.getData.bind(this);
         this.state = {
             display: [],
             ave: 0,
-            review: 0
+            total: 0,
+            roast: "",
         };
     }
     componentDidMount(){
+        this.getData();
+    }
+    getData(){
         let ave = 0;
         axios.get(this.url + this.props.match.params.id)
             .then((response) => {
@@ -23,16 +30,34 @@ class TeacherPage extends Component{
                 const items = (response.data.map((x) =>
                     <li key={x._id}>{x.toast}</li>
                 ));
-                this.setState({display: items, review: (<Review number={ave/response.data.length}/>)})
+                const rev = ave/response.data.length;
+                console.log(rev);
+                if(isNaN(rev)){
+                    this.setState({display: items, total: (<Review number={0}/>), refer: this.props.match.params.id})
+                }
+                else{
+                    this.setState({display: items, total: (<Review number={rev}/>), refer: this.props.match.params.id})
+                }
             })
             .catch(function (error) {
                 console.log(error);
             });
     }
-    handleSubmit(x) {
-        axios.post('https://roast-my-teacher-backend.herokuapp.com/api/roasts/', {refer: this.props.match.params.id, review: "", toast: ""})
+    handleChange(e) {
+        const target = e.target;
+        const name = target.name;
+        const value = target.value;
+        this.setState({
+            [name]: value
+        });
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        axios.post('https://roast-my-teacher-backend.herokuapp.com/api/roasts/', {refer: this.state.refer, review: this.state.review, toast: this.state.roast})
             .then(res => {
-                //this.setState({ data: res });
+                console.log(res);
+                console.log(res.data);
+                this.getData();
             })
             .catch(err => {
                 console.error(err);
@@ -41,14 +66,18 @@ class TeacherPage extends Component{
     render() {
         return (
             <div>
-                {this.state.review}
+                {this.state.total}
                 <ul className="list-group" id="table">
                     {this.state.display}
                 </ul>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <label>
-                        Name:
-                        <input type="text" name="name" />
+                        Review:
+                        <input type="number" name="review" onChange={this.handleChange}/>
+                    </label>
+                    <label>
+                        Roast:
+                        <input type="text" name="roast" onChange={this.handleChange}/>
                     </label>
                     <input type="submit" value="Submit" />
                 </form>
